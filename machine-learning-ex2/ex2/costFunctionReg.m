296dad85f1f329b6d148e9cb1009a77c7d46c269
jorgefpont <jorgefpont@gmail.com>
2018-10-07 18:50:21 -0700

    confused n where I am ...

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,6 +17,38 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% h = g(z) = g(X*theta) -- vectorized implementation of h(X)
+z = X * theta;
+h = (1+exp(-z)).^-1;
+
+% J(theta) = (1/m) * ( (-yT log(h)) - (1-y)T log(1-h) ) + 
+%     + lambda/(2m) * sum (theta ^2) ==> added term
+
+J = (1/m) * ...
+    (   ( transpose(-y) * log(h) ) - ...
+        ( transpose(1-y) * log(1-h) ) ...
+    ) + ...
+    (lambda/(2*m)) * sum(theta(2:length(theta)) .^2);  % new term
+
+% grad_J = (1/m) * XT * (h - y) /// for theta 0
+tempgrad = (1/m) * ...
+    transpose(X) * ...
+    (h - y);
+    
+% gradient for theta 0
+grad(1) = tempgrad(1);
+
+tempgrad = (1/m) * ...
+    transpose(X) * ...
+    (h - y) + ...
+    (lambda/m) * theta;   % new term
+
+    % gradient for rest of theta terms
+grad(2:length(theta)) = tempgrad(2:size(theta));
+   
+
+    
+    
 
 
 
